97cb03b8f05d8d5285cf1f0eef7d6ac9c9d43a05
Stryker Stinnette <strykerkent@gmail.com>
2021-07-02 19:25:54 -0500

    added tests for App, AddOpportunity, and OpportunityTable components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import ReactTestUtils from 'react-dom/test-utils';
+import { renderHook, act } from 'react-dom/test-utils';
 import App from './App';
+import AddOpportunity from './components/AddOpportunity'
+import OpportunityTable from './components/OpportunityTable'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
+test('check if navbar loaded in App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const navbarTitle = screen.getByText(/React Hooks Opportunity CRUD/i);
+  expect(navbarTitle).toBeInTheDocument();
 });
+
+test('check if App component loaded', () => {
+  render(<App />);
+  const container = screen.getByText('Opportunities')
+  expect(container).toBeInTheDocument();
+});
+
+test('check if OpportunityTable component loaded', () => {
+  const fakeData = [
+    { id: 1, name: 'TEST1', account: 'test', location: 'test' },
+    { id: 2, name: 'TEST2', account: 'test', location: 'test' }
+  ]
+  render(<OpportunityTable opportunities={fakeData}/>);
+  expect(document.querySelector('table')).toBeInTheDocument()
+})
+
+test('check if AddOpportunity component loaded', () => {
+  render(<AddOpportunity />);
+  expect(document.querySelector('#name')).toBeInTheDocument()
+})
