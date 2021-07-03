89b87b5b190d4a0c70b0f6d223bb0d7d68e7b89a
Stryker Stinnette <strykerkent@gmail.com>
2021-07-02 19:31:09 -0500

    removed unused imports from app.test page and cleaned up wording of tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import ReactTestUtils from 'react-dom/test-utils';
-import { renderHook, act } from 'react-dom/test-utils';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 import AddOpportunity from './components/AddOpportunity'
 import OpportunityTable from './components/OpportunityTable'
-import userEvent from '@testing-library/user-event'
 
-test('check if navbar loaded in App component', () => {
-  render(<App />);
-  const navbarTitle = screen.getByText(/React Hooks Opportunity CRUD/i);
-  expect(navbarTitle).toBeInTheDocument();
-});
+test('Navbar rendered in App component', () => {
+  render(<App />)
+  const navbarTitle = screen.getByText(/React Hooks Opportunity CRUD/i)
+  expect(navbarTitle).toBeInTheDocument()
+})
 
-test('check if App component loaded', () => {
-  render(<App />);
+test('App component rendered', () => {
+  render(<App />)
   const container = screen.getByText('Opportunities')
-  expect(container).toBeInTheDocument();
-});
+  expect(container).toBeInTheDocument()
+})
 
-test('check if OpportunityTable component loaded', () => {
+test('OpportunityTable component rendered', () => {
   const fakeData = [
     { id: 1, name: 'TEST1', account: 'test', location: 'test' },
     { id: 2, name: 'TEST2', account: 'test', location: 'test' }
   ]
-  render(<OpportunityTable opportunities={fakeData}/>);
+  render(<OpportunityTable opportunities={fakeData}/>)
   expect(document.querySelector('table')).toBeInTheDocument()
 })
 
-test('check if AddOpportunity component loaded', () => {
-  render(<AddOpportunity />);
+test('AddOpportunity component rendered', () => {
+  render(<AddOpportunity />)
   expect(document.querySelector('#name')).toBeInTheDocument()
 })
